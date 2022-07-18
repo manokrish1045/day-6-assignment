03d5d2bc9a45e2ab8eeae9aca1dda49a54be9e1f
manokrish1045 <109267208+manokrish1045@users.noreply.github.com>
2022-07-18 19:54:21 +0530

    Add files via upload

---
+++ 2script.js
@@ -0,0 +1,11 @@
+class circle{
+    const pi= 3.1421;
+    constructor(radius,color){
+
+    this.radius=radius;
+    this.color=color;
+    }
+}
+getarea(){
+    returnpi*this.radius**this.radius
+}
---
+++ script.js
@@ -0,0 +1,11 @@
+class Movie{
+    constructor(title,studio,rating){
+       this.title=title;
+       this.studio=studio;
+       this.rating=rating;
+    }
+    
+   }
+    var mov= new Movie("casino royale","EON production","PG13");
+
+   console.log(mov);
